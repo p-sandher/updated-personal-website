1bc660cb1d122d21acae68d265ecfa4b3aeb4eb2
p-sandher <puneetsandher@gmail.com>
2024-04-22 18:10:18 -0400

    first commit

---
+++ src/models/Plane.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+
+import planeScene from "../assets/3d/plane.glb";
+
+// 3D Model from: https://sketchfab.com/3d-models/stylized-ww1-plane-c4edeb0e410f46e8a4db320879f0a1db
+export function Plane({ isRotating, ...props }) {
+  const ref = useRef();
+
+  const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+    console.log({isRotating});
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+  }, [actions, isRotating]);
+
+  return (
+    <mesh {...props} ref={ref}>
+      // use the primitive element when you want to directly embed a complex 3D
+      model or scene
+      <primitive object={scene} />
+    </mesh>
+  );
+}
+
+export default Plane
