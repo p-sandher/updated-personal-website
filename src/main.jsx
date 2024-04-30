bd6bd13a515f9f3d825bd65698617d26b6f3cdac
p-sandher <puneetsandher@gmail.com>
2024-04-30 17:14:51 -0400

    updated index.html

--- src/main.jsx
+++ src/main.jsx
@@ -7,4 +7,6 @@ import './index.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />)
+
+
 // ReactDom.createRoot(document.getElementById('root')).render(<App />);
